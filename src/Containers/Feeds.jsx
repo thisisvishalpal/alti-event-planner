17cfd86fb4989dbe5315bd7d3d23bf7fc3e3638c
Vishal Pal <vishalpal3073@gmail.com>
2025-01-23 15:25:52 +0530

    multilingual logged in example

--- src/Containers/Feeds.jsx
+++ src/Containers/Feeds.jsx
@@ -2,12 +2,14 @@ import React, { useEffect } from "react";
 
 import { useDispatch } from "react-redux";
 import { Container } from "react-bootstrap";
+import { useTranslation } from "react-i18next";
 
 import { UploadSection, FeedSection } from "Components";
 import { fetchUserFeeds } from "Slices";
 
 export const Feeds = () => {
   const dispatch = useDispatch();
+  const { t } = useTranslation();
 
   useEffect(() => {
     window.scrollTo({ top: 0, behavior: "auto" });
@@ -20,7 +22,7 @@ export const Feeds = () => {
   return (
     <Container className="mt-3">
       <UploadSection />
-      <h3 className="mb-4">Feeds</h3>
+      <h3 className="mb-4">{t("routes.feeds.title")}</h3>
       <FeedSection />
     </Container>
   );
